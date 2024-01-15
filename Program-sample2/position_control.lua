06370d6acbadbe11510af5b39c543f1f5fce57d6
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-01-15 11:43:26 +0800

    Sample2

---
+++ Program-sample2/position_control.lua
@@ -0,0 +1,36 @@
+-- Objects: cuboid, revolute joint, cylinder
+
+function sysCall_init()
+    sim = require('sim')
+    joint=sim.getObjectHandle('Revolute_joint')
+    last_time=sim.getSimulationTime()
+    ccw=false
+    -- do some initialization here
+end
+
+function sysCall_actuation()
+    -- put your actuation code here
+    if ((sim.getSimulationTime()-last_time)>50) then
+        time_elapsed=true
+        last_time=sim.getSimulationTime()
+    else
+        time_elapsed=false
+    end
+    if (time_elapsed and ccw==false) then
+        ccw=false
+        sim.setJointTargetPosition(joint,90*math.pi/180)
+    elseif (time_elapsed and ccw==false) then
+        ccw=true
+        sim.setJointTargetPosition(joint,-90*math.pi/180)
+    end
+end
+
+function sysCall_sensing()
+    -- put your sensing code here
+end
+
+function sysCall_cleanup()
+    -- do some clean-up here
+end
+
+-- See the user manual or the available code snippets for additional callback functions and details
--- README.md
+++ README.md
@@ -12,4 +12,16 @@ Testing:
 ![velo gif](https://github.com/MikkoDT/Coppeliasim_Learning_2024/assets/93197249/b108db37-de16-4764-a440-741c59097e1e)
 
      
+**Programming Sample 2**
 
+Position Control
+
+    Testing: 
+        function sysCall_init()
+        sim = require('sim')
+        joint=sim.getObjectHandle('Revolute_joint')
+        last_time=sim.getSimulationTime()
+        ccw=false
+
+    end
+    
