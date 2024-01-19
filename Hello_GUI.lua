21423eba84a68853560000a752f790b55abdc4fc
Mikko De Torres <mikko.detorres@g.batstate-u.edu.ph>
2024-01-19 23:26:53 +0800

    Update Hello_GUI.lua

--- Hello_GUI.lua
+++ Hello_GUI.lua
@@ -1,16 +1,14 @@
 if (sim_call_type==sim.syscb_init) then
     xml = [[
-    <ui closeable="true" on-close="closeEventHandler" resizable="true">
-    <label text="DYOR"  wordwrap="true" />
-    <label text="Linear Speed" />
+    <ui closeable="true" on-close="closeEventHandler" resizable="true" title="Hello World!">
+    <label text="Hello World!"/>
     </ui>
     ]]
     
     ui=simUI.create(xml)
     
     progress=0
-    
-end    
+end
 if (sim_call_type==sim.syscb_cleanup) then
     simUI.destroy(ui)
 end
